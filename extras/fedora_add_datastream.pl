bf7fd10c988f1e0a1db7e29505c00cbcac2f16b3
Mike Lynch <mike.lynch@iinet.net.au>
2013-06-07 11:32:09 +1000

    Changes while testing URL datastreams

--- extras/fedora_add_datastream.pl
+++ extras/fedora_add_datastream.pl
@@ -11,8 +11,12 @@ my %PARAMS = (
 	password => 'hce39alp'
 );
 
+
+
 my $PID = 'RDC:1';
-my $FILE = '/home/mike/workspace/RDC Data Capture/src/t/Fixtures/Capture/20130404_P1_E1_UTS Demo Microscope_Testable_JoeTest/Paramecium.jpg';
+my $FILE = '/home/mike/workspace/RDC Data Capture/src/t/Fixtures/Capture/20130404_P1_E1/200_Noctiluca_scintillans_unica.jpg';
+my $URL = 'http://localhost:8080/repository/local/RDC:115/Paramecium.jpg';
+my $REAL_URL = 'https://www.dropbox.com/s/3yq83vjaozg16p9/Terra.tif';
 
 my $fc = Catmandu::FedoraCommons->new(
 	$PARAMS{baseurl}, $PARAMS{usename}, $PARAMS{password}
@@ -20,22 +24,42 @@ my $fc = Catmandu::FedoraCommons->new(
 	die("Can't create Catmandu::FedoraCommons instance");
 };
 
-my $result = $fc->addDatastream(
+
+add_datastream(
 	'pid' => 'RDC:45',
- 	'file' => '/home/mike/workspace/RDC Data Capture/src/t/Test/Capture/20130404_P1_E1_UTS Demo Microscope_Testable_JoeTest/220px-Noctiluca_scintillans_unica.jpg',
+ 	'file' => $FILE,
+ 	'dsLabel' => 'Noctiluca',
+ 	'mimeType' => 'image/jpeg',
     'dsID' => "Datastream1"
 );
- 
-#
-#	pid => $PID,
-#	dsID => 'Paramecium',
-#	file => $FILE,
-#	dsLabel => 'Paramecium wiggedy',
-#	mimeType => 'image/jpeg'
-#);
 
-if( $result->is_ok ) {
-	print Dumper({result => $result->parse_content}) . "\n";
-} else {
-	print $result->error . "\n";
-} 
+add_datastream(
+	'pid' => 'RDC:45',
+ 	'url' => $URL,
+ 	'dsLabel' => 'Paramecium',
+ 	'mimeType' => 'image/jpeg',
+    'dsID' => "Datastream2"
+);
+
+add_datastream(
+	'pid' => 'RDC:45',
+ 	'url' => $REAL_URL,
+ 	'dsLabel' => 'FSVO',
+ 	'mimeType' => 'image/tiff',
+    'dsID' => "Datastream3"
+);
+
+sub add_datastream {
+	my %params = @_;
+
+	my $result = $fc->addDatastream(%params);
+ 
+	if( $result->is_ok ) {
+		print "Request worked.\n";
+	} else {
+		print $result->error . "\n";
+		print "Result: $result\n";
+		print Dumper({result => $result}) . "\n";
+	}
+}
+
