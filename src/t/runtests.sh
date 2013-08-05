8bb664571764b6071ef090bc03907d0feac6e72a
Mike Lynch <mike.lynch@iinet.net.au>
2013-08-05 10:39:34 +1000

    RELEASE v1.0 Collagen

--- src/t/runtests.sh
+++ src/t/runtests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# NOTE: ./t/Test will get overwritten with the fixtures from t/Fixtures
+# before any of the scripts runs. So any changes to config files need
+# to be made to ./t/Fixtures/.. not ./t/Test/..
+
 export COATAGLUE_CONFIG=./t/Test/Config/CoataGlue.cf
 export COATAGLUE_FIXTURES=./t/Fixtures
 export COATAGLUE_LOG4J=./t/log4j.properties
