427c91c3407da96bf88aca63a141ab0fc9a8a3dc
Mike Lynch <mike.lynch@iinet.net.au>
2013-08-29 10:34:34 +1000

    Made the harvest wrapped scripts a bit more transportable

--- src/harvest.sh
+++ src/harvest.sh
@@ -1,18 +1,11 @@
 #/bin/bash
 
-# a version of the harvest runner which builds the fixtures (as with the
-# t/ regression tests)
-
-
-
 export COATAGLUE_HOME=/home/mike/workspace/RDC\ Data\ Capture
 export COATAGLUE_PERLLIB=$COATAGLUE_HOME/src/lib
 export COATAGLUE_LOG4J=$COATAGLUE_HOME/src/t/log4j.properties
 export COATAGLUE_CONFIG=$COATAGLUE_HOME/src/t/Test/Config/CoataGlue.cf
 export COATAGLUE_SOURCES=$COATAGLUE_HOME/src/t/Test/Config/DataSources.cf
 export COATAGLUE_TEMPLATES=$COATAGLUE_HOME/src/t/Test/Config/Templates
-
-./t/fixtures.pl
 
 ./harvest.pl
 
