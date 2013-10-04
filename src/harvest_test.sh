3767b0ddb5162817fc205fdcc548a8c9645da264
Mike Lynch <mike.lynch@iinet.net.au>
2013-10-04 13:35:04 +1000

    Committing local settings for harvest scripts

--- src/harvest_test.sh
+++ src/harvest_test.sh
@@ -9,18 +9,18 @@
 
 
 
-export COATAGLUE_HOME=/home/mike/workspace/RDC\ Data\ Capture
-export COATAGLUE_PERLLIB=$COATAGLUE_HOME/src/lib
-export COATAGLUE_LOG4J=$COATAGLUE_HOME/src/t/log4j.properties
-export COATAGLUE_CONFIG=$COATAGLUE_HOME/src/t/Test/Config/CoataGlue.cf
-export COATAGLUE_SOURCES=$COATAGLUE_HOME/src/t/Test/Config/DataSources.cf
-export COATAGLUE_TEMPLATES=$COATAGLUE_HOME/src/t/Test/Config/Templates
+export COATAGLUE_HOME=/home/mikelynch/DataCapture
+export COATAGLUE_PERLLIB=/home/mikelynch/CoataGlue/src/lib
+export COATAGLUE_LOG4J=$COATAGLUE_HOME/log4j.properties
+export COATAGLUE_CONFIG=$COATAGLUE_HOME/Config/CoataGlue.cf
+export COATAGLUE_SOURCES=$COATAGLUE_HOME/Config/DataSources.cf
+export COATAGLUE_TEMPLATES=$COATAGLUE_HOME/Config/Templates
 
 
 while getopts ":f" opt; do
     case $opt in
         f)
-            ./t/fixtures.pl
+            /home/mikelynch/CoataGlue/src/t/fixtures.pl
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
@@ -29,5 +29,4 @@ while getopts ":f" opt; do
 done
 
 
-./harvest.pl -t
-
+/home/mikelynch/CoataGlue/src/harvest.pl -t
