671cc69147816de81536d82f223622e7d5111dd9
Mike Lynch <mike.lynch@iinet.net.au>
2014-04-01 11:37:33 +1100

    Added the solr test script to git

---
+++ extras/test_solr.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use strict;
+use Data::Dumper;
+
+use Apache::Solr;
+
+my $SOLR = 'https://redbox.research.uts.edu.au/redbox-solr';
+my $CORE = 'fascinator';
+
+my $ID = '69e60340b7918f8e3222eac87e5e5b50';
+my $RDCURL = 'http\://data.research.uts.edu.au/dataset/au.edu.uts780';
+my $SEARCHFIELD = 'bibo\:DataLocation.1.dc\:location';
+
+print "Trying Apache::Solr\n";
+
+my $solr = Apache::Solr->new(
+    server => $SOLR,
+    core => $CORE,
+    ) || die;
+
+# "id:$ID"
+
+#do_search('id', $ID);
+
+do_search($SEARCHFIELD, $RDCURL);
+
+
+
+sub do_search {
+    my ( $field, $search ) = @_;
+
+    my $q = join(':', $field, $search);
+    print "Searching $q\n";
+    
+    my $results = $solr->select(q => $q);
+    
+    my $n = $results->nrSelected;
+    
+    print "Got $n results.";
+    
+    my $doc = $results->selected(0);
+    
+    if( $doc ) {
+        
+        print "Title: " . $doc->content('dc_title') . "\n";
+        print "Access: " . $doc->content('dc_accessRights') . "\n";
+        
+        my @names = $doc->fieldNames();
+        print join("\n", map { $_ . ":" . $doc->content($_) } @names );
+        print "\n";
+        
+    } else {
+        print "Document not found in Solr.\n";
+    }
+}
