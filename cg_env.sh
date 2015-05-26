b8c78838a14ee901d316638c2b72931857f3f7dd
Mike Lynch <mike.lynch@iinet.net.au>
2015-05-27 09:12:00 +1000

    Added sample shell scripts for the CG environment variables and for running the harvester.

---
+++ cg_env.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+export COATAGLUE_HOME=/The_directory_this_file_is_in
+export COATAGLUE_CODE=/The_path_containing_CoataGlue_src
+export COATAGLUE_PERLLIB=$COATAGLUE_CODE/src/lib
+export COATAGLUE_LOG4J=$COATAGLUE_HOME/log4j.properties
+export COATAGLUE_CONFIG=$COATAGLUE_HOME/Config/CoataGlue.cf
+export COATAGLUE_SOURCES=$COATAGLUE_HOME/Config/DataSources.cf
+export COATAGLUE_TEMPLATES=$COATAGLUE_HOME/Config/Templates
+
+
